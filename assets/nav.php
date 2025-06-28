0e6635f54fa2189848b05ddf9c4be939d20f7c1e
Alex Barroso <abarros@purdue.edu>
2025-06-28 07:10:27 -0400

    Mejora del navbar

--- assets/nav.php
+++ assets/nav.php
@@ -1,12 +1,12 @@
 <!-- Navigation Bar -->
   <nav class="navbar navbar-expand-lg px-4 py-3">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">AISC</a>
+      <a class="navbar-brand w-50" href="#">AISC</a>
       <button class="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
+      <div class="collapse navbar-collapse d-flex justify-content-between w-50" id="navbarNav">
+        <ul class="navbar-nav d-flex justify-content-around" style="width:75%">
           <li class="nav-item">
             <a class="nav-link" href="#">Inicio</a>
           </li>
