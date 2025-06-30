0d343b08b29097d866893774f704a444e0fb2667
Hugo C <hcienteno@gmail.com>
2025-06-30 20:22:51 +0200

    Faded footer version

--- assets/footer.php
+++ assets/footer.php
@@ -1,8 +1,7 @@
 <footer>
   <div class="footer-container">
     <div class="footer-left">
-      <a href="#top" class="footer-link">Home</a>
-      <a href="#" class="footer-link">Contact</a>
+      <img src="images/SVG/Logo AISC Madrid.svg" alt="Logo" style="width:40px; height:auto; vertical-align:middle; margin-right:8px;">
     </div>
     <div class="footer-right">
       <a href="https://www.instagram.com/aisc_madrid/" target="_blank" class="footer-icon-wrapper">
@@ -10,6 +9,9 @@
       </a>
       <a href="https://www.linkedin.com/company/ai-student-collective-madrid" target="_blank" class="footer-icon-wrapper">
         <i class="bi bi-linkedin footer-icon"></i>
+      </a>
+      <a href="https://chat.whatsapp.com/DHdetGyDbvnAMMKo0JaaqY?mode=r_c" target="_blank" class="footer-icon-wrapper">
+        <i class="bi bi-whatsapp footer-icon"></i>
       </a>
     </div>
   </div>
