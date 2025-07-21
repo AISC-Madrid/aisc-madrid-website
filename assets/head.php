a7766eec5ebedb0aa53be3442b4289a39c5f009d
Alex Barroso <abarros@purdue.edu>
2025-07-21 18:12:11 +0200

    Update head.php

--- assets/head.php
+++ assets/head.php
@@ -21,6 +21,8 @@
 
     <!-- Optional: Custom CSS -->
     <link rel="stylesheet" href="css/styles.css">
+
+    <base href="/" />
     
     <!-- Icons -->
      <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
