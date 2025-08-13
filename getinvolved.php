010e308b85ec5a33a4f8060c82f757520e998e74
Hugo C <hcienteno@gmail.com>
2025-08-13 12:04:11 +0200

    Create get involved section

---
+++ getinvolved.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include("assets/head.php"); ?>
+
+<body class="d-flex flex-column min-vh-100">
+
+  <?php include("assets/nav.php"); ?>
+    <div class="container scroll-margin">
+        <p style="color: black">Get involved at AISC Madrid</p>
+    </div>
+    <?php include('assets/footer.php'); ?>
+
+    <script src="js/language.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
