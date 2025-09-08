549711b5f25057c54e1700b7eaf530bc2e4d5f8a
Alex Barroso <abarros@purdue.edu>
2025-09-08 21:31:51 +0200

    Update newsletter.php

--- mails/newsletter.php
+++ mails/newsletter.php
@@ -107,16 +107,6 @@ include("../assets/db.php");
 
     <div class="container-fluid" style="margin-top:90px;">
         <div class="row">
-            <!-- Sidebar -->
-            <nav class="col-md-2 d-none d-md-block bg-light sidebar py-4" style="min-height:100vh;">
-                <div class="nav flex-column">
-                    <a class="nav-link" href="../dashboard.php"><i class="bi bi-speedometer2"></i> Resumen</a>
-                    <a class="nav-link" href="../users.php"><i class="bi bi-people"></i> Usuarios</a>
-                    <a class="nav-link active" href="newsletter.php"><i class="bi bi-envelope"></i> Newsletter</a>
-                    <a class="nav-link" href="../events/events_list.php"><i class="bi bi-calendar-event"></i> Eventos</a>
-                </div>
-            </nav>
-
             <!-- Main content -->
             <main class="col-md-10 ms-sm-auto col-lg-10 px-md-4 py-4">
                 <h2 class="mb-4">Enviar Newsletter</h2>
@@ -303,7 +293,6 @@ include("../assets/db.php");
 
                         $conn->close();
 
-                        $conn->close();
                         ?>
                     </div>
                 </div>
@@ -311,7 +300,7 @@ include("../assets/db.php");
         </div>
     </div>
 
-    <?php include("../assets/footer.php"); ?>
+    <?php include('..assets/footer.php'); ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
