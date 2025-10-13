a42cce08da7ffa08447a9825372d8e44128157fb
centenohugo <hcienteno@gmail.com>
2025-10-13 22:56:15 +0200

    Fix missing symbol

--- events/evento.php
+++ events/evento.php
@@ -208,7 +208,7 @@ if (!$event) {
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="..js/navbar.js"></script>
+    <script src="../js/navbar.js"></script>
     <script src="../js/language.js"></script>
 </body>
 
