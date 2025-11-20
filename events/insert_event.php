0ac40ffae4f01efc691867cbc6a89bcc827c5ac0
Alejandro Barroso Bueso <abarros@purdue.edu>
2025-11-20 16:21:27 +0100

    Fix parameter binding string in insert_event.php

--- events/insert_event.php
+++ events/insert_event.php
@@ -23,7 +23,7 @@ $stmt = $conn->prepare($sql);
 if (!$stmt) die("Error al preparar la consulta: " . $conn->error);
 
 $stmt->bind_param(
-    "ssssssssssssi",
+    "sssssssssssi",
     $_POST['title_es'],
     $_POST['title_en'],
     $_POST['type_es'],
