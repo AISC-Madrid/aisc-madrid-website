85d71c0079fe435a9629c3d0e9c6eafba9ae3027
chrstncrrnd <chrstncrrnd@gmail.com>
2026-04-06 18:01:42 +0200

    new json-based translation system

--- js/language.js
+++ js/language.js
@@ -10,10 +10,40 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const changeLanguage = (lang) => {
         if (languageBtn) languageBtn.textContent = lang.toUpperCase();
+        
         document.querySelectorAll('[data-en]').forEach(el => {
-            const html = el.dataset[lang]; // e.g., "&lt;strong&gt;Hello&lt;/strong&gt;"
+            const html = el.dataset[lang]; // e.g., "<strong>Hello</strong>"
             if (html) el.innerHTML = decodeHtml(html); // now <strong> works
         });
+
+        // note: we can only translate leaf nodes. So for elements whose children aren't just 
+        // text, the text to be translated in <span> and the span should include the 
+        // translation key instead
+        // eg:
+        // <ol translation-key="title"> List title 
+        //   <li translation-key="elem"> List element </li>
+        // ...
+        // </ol>
+        // should be:
+        // <ol> <span translation-key="title"> List title </span>
+        //   <li translation-key="elem"> List element </li>
+        // ...
+        // </ol>
+        fetch(`/translations/${lang}.json`)
+            .then(res => {
+                if (res.ok) return res.json();
+                throw new Error(`Translations for ${lang} not found or failed to load`);
+            })
+            .then(translations => {
+                document.querySelectorAll('[translation-key]').forEach(el => {
+                    const key = el.getAttribute('translation-key');
+                    if (translations && translations[key]) {
+                        el.innerHTML = decodeHtml(translations[key]); // <strong> works here too
+                    }
+                });
+            })
+            .catch(err => console.warn('Translation warning:', err));
+
         localStorage.setItem('lang', lang);
     };
 
