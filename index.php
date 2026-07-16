592b37c902a14a8380ccb501dc2a488d90f2a2b1
Alejandro Barroso Bueso <178938942+abarroso08@users.noreply.github.com>
2026-07-17 01:10:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
+
 <?php
 
 include("assets/csrf.php");
@@ -48,6 +47,8 @@ if ($needed_from_past > 0) {
 }
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
 
 <?php
 include("assets/head.php");
